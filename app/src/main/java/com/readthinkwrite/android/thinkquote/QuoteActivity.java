c29b3ece0cac4058ea6ebecd69b51f8d7af35090
USER <portillom@g.cofc.edu>
2019-01-01 20:26:44 -0800

    Adding more string quotes

--- app/src/main/java/com/readthinkwrite/android/thinkquote/QuoteActivity.java
+++ app/src/main/java/com/readthinkwrite/android/thinkquote/QuoteActivity.java
@@ -77,7 +77,7 @@ public class QuoteActivity extends AppCompatActivity {
         setButton();
     }
 
-    //The Fisher-Yates shuffle bwfjk
+    //The Fisher-Yates shuffle
     private void shuffleQuoteOrder(Quote[] mQuoteBank){
         int index;
         Quote temp;
